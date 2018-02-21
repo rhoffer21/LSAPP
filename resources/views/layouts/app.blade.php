8c07c3a909ccad61927d36b6c47638d66135b72f
Ryan <rhoffer21@gmail.com>
2018-02-21 13:48:42 -0500

    Created a Navbar and added it to the templater. edited the index to have the login and register buttons.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
         <title>{{config('app.name', 'LSAPP')}}</title>
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+         @yield('content')
+        </div>
     </body>
 </html>
