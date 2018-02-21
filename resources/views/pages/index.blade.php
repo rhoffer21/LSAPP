0336fa7f9d69cc1d232cf4e54f3f2b6263451fb9
Ryan <rhoffer21@gmail.com>
2018-02-21 10:47:19 -0500

    Setup a template and rewrote index about and services to use the template.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
+@section('content')
     <h1>Welcome to Laravel</h1>
     <p>This is the laravel application from the Laravel from Scratch youtube series.</p>
-    </body>
-</html>
+@endsection
