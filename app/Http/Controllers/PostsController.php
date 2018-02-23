dbb4f113c5a950d994802a89c367fae46f6d7460
Ryan <rhoffer21@gmail.com>
2018-02-23 11:32:15 -0500

    Added pagination to the posts page.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('title','desc')->paginate(10);
         return view('posts.index')->with('posts', $posts);
     }
 
